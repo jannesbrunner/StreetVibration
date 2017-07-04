eaa1928d45f1105c38e99819cf035c502f5bfe20
jannesbrunner <mail@jannebrunner.de>
2017-07-04 09:55:17 +0200

    Implement code for gyro sensor

--- src/main/java/jannesbrunner/htwberlin/com/streetvibration/MainActivity.java
+++ src/main/java/jannesbrunner/htwberlin/com/streetvibration/MainActivity.java
@@ -1,11 +1,16 @@
 package jannesbrunner.htwberlin.com.streetvibration;
 
 import android.content.Intent;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- src/main/java/jannesbrunner/htwberlin/com/streetvibration/Metering.java
+++ src/main/java/jannesbrunner/htwberlin/com/streetvibration/Metering.java
@@ -1,7 +1,23 @@
 package jannesbrunner.htwberlin.com.streetvibration;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class Metering extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor gyro;
+    private TextView output;
+    private TextView log;
 
 public class Metering extends AppCompatActivity {
 
